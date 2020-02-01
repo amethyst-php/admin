9f0bfdae8211c30c219e68f32c6a156c2250e321
railken <railken@protonmail.ch>
2020-02-01 19:31:49 +0100

    removing http

--- src/Http/Controllers/Admin/IndexController.php
+++ src/Http/Controllers/Admin/IndexController.php
@@ -21,13 +21,7 @@ class IndexController extends RestController
 
     public function retrieveInfo()
     {
-        $events = [];
-        $dataBuilders = [];
-
-        $amethyst = ['data' => $this->retrieveData()];
-
-        $lang = [];
-
+        $amethyst = [];
         $helper = new \Amethyst\Core\Helper();
 
         foreach (glob(resource_path('/lang/vendor/*')) as $pathPackage) {
@@ -54,77 +48,7 @@ class IndexController extends RestController
         }
 
         return array_merge($amethyst, [
-            'lang'      => $lang,
-            'discovery' => [
-                'events'        => $events,
-                'data_builders' => $dataBuilders,
-            ],
+            'lang'      => $lang
         ]);
-    }
-
-    public function retrieveData()
-    {
-        $helper = new \Amethyst\Core\Helper();
-
-        return $helper->getData()->map(function ($data) use ($helper) {
-            $name = $helper->getNameDataByModel(Arr::get($data, 'model'));
-            $manager = app(Arr::get($data, 'manager'));
-
-            return [
-                'name'       => $name,
-                'attributes' => $manager->getAttributes()->map(function ($attribute) use ($manager) {
-                    return $this->retrieveAttribute($manager, $attribute);
-                })->toArray(),
-                'relations' => collect(app('eloquent.mapper')->getFinder()->relations(Arr::get($data, 'model')))->map(function ($relation, $key) use ($helper) {
-                    $relation = (object) $relation;
-
-                    $return = [
-                        'key'   => $relation->key,
-                        'type'  => $relation->type,
-                        'data'  => $helper->getNameDataByModel($relation->model),
-                        'scope' => app('amethyst')->parseScope($relation->model, $relation->scope),
-                    ];
-
-                    if (isset($relation->intermediate)) {
-                        $return = array_merge($return, [
-                            'intermediate'    => $helper->getNameDataByModel($relation->intermediate),
-                            'foreignPivotKey' => $relation->foreignPivotKey,
-                            'relatedPivotKey' => $relation->relatedPivotKey,
-                        ]);
-                    }
-
-                    return $return;
-                })->values(),
-                'descriptor' => app(Arr::get($data, 'manager'))->getDescriptor(),
-            ];
-        })->values()->toArray();
-    }
-
-    public function retrieveAttribute($manager, $attribute)
-    {
-        $params = [
-            'name'       => $attribute->getName(),
-            'type'       => $attribute->getType(),
-            'fillable'   => (bool) $attribute->getFillable(),
-            'required'   => (bool) $attribute->getRequired(),
-            'unique'     => (bool) $attribute->getUnique(),
-            'hidden'     => (bool) $attribute->getHidden(),
-            'default'    => $attribute->getDefault($manager->newEntity()),
-            'descriptor' => $attribute->getDescriptor(),
-        ];
-
-        if ($attribute instanceof Attributes\EnumAttribute) {
-            $params = array_merge($params, [
-                'options' => $attribute->getOptions(),
-            ]);
-        }
-
-        if ($attribute instanceof Attributes\BelongsToAttribute || $attribute instanceof Attributes\MorphToAttribute) {
-            $params = array_merge($params, [
-                'relation' => $attribute->getRelationName(),
-            ]);
-        }
-
-        return $params;
     }
 }
